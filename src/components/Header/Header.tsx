14571660f29b6faf8221b534c4fc681a07657999
Vivek Rokkam <vivekrokkam@gmail.com>
2019-06-16 16:23:43 +1200

    initial

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className='headerText'>Find the Age. </h1>
+        </div>
+    )
+}
